0698909ca18b4f5aee7e72a02ab4c9df2c4e6a72
upendraupi <upendrakuruma@gmail.com>
2024-01-03 15:51:02 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,6 @@
+from csk.views import *
+from django.urls import Path
+app_name='thala'
+urlpatterns=[
+    path("dhoni/",dhoni,name="dhoni"),
+]
---
+++ ipl/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def virot(request):
+    return HttpResponse('best batsman')
